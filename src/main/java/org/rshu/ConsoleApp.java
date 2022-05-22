7992ca7a3a26176e61525a8014df00c0e767a3d3
Denis Shabashov <countetnum404@gmail.com>
2022-05-22 03:06:28 +0300

    1. File input was added 2. App class was added

---
+++ src/main/java/org/rshu/ConsoleApp.java
@@ -0,0 +1,74 @@
+package org.rshu;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class ConsoleApp {
+
+    public ConsoleApp()
+    {
+        this.scanner = new Scanner(System.in);
+        this.graph = new Graph();
+    }
+    public void run()
+    {
+        System.out.println("Read edges from console/file?\n 1. console \n 2. file");
+        String result = this.scanner.next();
+        if (result.equals(Options.consoleOption.getOption())) {
+            this.consoleRead();
+        }
+        if (result.equals(Options.fileOption.getOption()))
+        {
+            this.fileRead();
+        }
+    }
+    protected void consoleRead()
+    {
+        String result = scanner.nextLine();
+        System.out.println("Fill the edges or... \nType 3 to finish filling the Graph");
+        while (!result.equals(Options.finishOption.getOption())) {
+            result = this.scanner.nextLine();
+            if (result.length() > 1) {
+                try {
+                    this.addEdge(result);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+        printSpanningTree();
+    }
+    protected void fileRead()
+    {
+        System.out.println("Filename/Path is required: ");
+        String result = this.scanner.next();
+        try
+        {
+            this.scanner = new Scanner(new File(result));
+            while(this.scanner.hasNextLine())
+            {
+                String readenLine = this.scanner.nextLine();
+                this.addEdge(readenLine);
+            }
+            this.printSpanningTree();
+        }
+        catch (FileNotFoundException fe)
+        {
+            System.out.println(fe.getMessage());
+        }
+    }
+    protected void printSpanningTree()
+    {
+        this.scanner.close();
+        this.spanningTree = new SpanningTree(this.graph);
+        System.out.println(this.spanningTree.getMinimalSpanningTreeString());
+    }
+    protected void addEdge(String edge)
+    {
+        String[] split = edge.split(" ");
+        this.graph.addEdge(new Edge(split[0], Integer.parseInt(split[1])));
+    }
+    protected Scanner scanner;
+    protected Graph graph;
+    protected SpanningTree spanningTree;
+}
